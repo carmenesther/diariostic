6b203aac34fae336205c099f21dd5c70f5555eca
Carmen Esther <32194588+carmenesther@users.noreply.github.com>
2020-11-08 13:58:28 +0100

    Cambio del título de la portada

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada actualizada",
-"autor" => "mtorres",
+"titulo" => "Portada actualizada x2",
+"autor" => "cfm465",
 "resumen" => "Resumen de la portada",
 ];
 ?>
